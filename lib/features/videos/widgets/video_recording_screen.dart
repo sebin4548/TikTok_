47d15f1b388f814a259d6be73c8ca0794ad8ccd6
sebin <sebin4548@gmail.com>
2023-08-19 17:06:48 +0900

    video permission

---
+++ lib/features/videos/widgets/video_recording_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,47 +1,48 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/onboarding/tutorial_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation.dart';
 import 'package:tiktok_clone/features/users/user_profile_screen.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
-    // nested Route
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => UsernameScreen(),
-          routes: [
-            GoRoute(
-                path: EmailScreen.routeURL,
-                name: EmailScreen.routeName,
-                builder: (context, state) {
-                  final args = state.extra as EmailScreenArgs;
-                  return EmailScreen(username: args.username);
-                },
-                routes: [
-                  GoRoute(
-                    path: "tutorial",
-                    name: "tutorial",
-                    builder: (context, state) => TutorialScreen(),
-                  ),
-                ]),
-          ],
-        )
-      ],
+      path: "/",
+      builder: (context, state) => VideoRecordingScreen(),
     ),
-    GoRoute(
-      path: "/mainNavigation",
-      name: "mainNavigation",
-      builder: (context, state) => MainNavigationScreen(),
-    ),
+
+    // //nested Route
+    // GoRoute(
+    //   name: SignUpScreen.routeName,
+    //   path: SignUpScreen.routeURL,
+    //   builder: (context, state) => SignUpScreen(),
+    //   routes: [
+    //     GoRoute(
+    //       path: UsernameScreen.routeURL,
+    //       name: UsernameScreen.routeName,
+    //       builder: (context, state) => UsernameScreen(),
+    //       routes: [
+    //         GoRoute(
+    //             path: EmailScreen.routeURL,
+    //             name: EmailScreen.routeName,
+    //             builder: (context, state) {
+    //               final args = state.extra as EmailScreenArgs;
+    //               return EmailScreen(username: args.username);
+    //             },
+    //             routes: [
+    //               GoRoute(
+    //                 path: "tutorial",
+    //                 name: "tutorial",
+    //                 builder: (context, state) => TutorialScreen(),
+    //               ),
+    //             ]),
+    //       ],
+    //     )
+    //   ],
+    // ),
+    // GoRoute(
+    //   path: "/mainNavigation",
+    //   name: "mainNavigation",
+    //   builder: (context, state) => MainNavigationScreen(),
+    // ),
 
     // GoRoute(
     //   name: "username_screen",
